f9906160c01bb023db126a7758e8f4b3e9f42e76
Philip Kristian Møller Flyvholm <phif@itu.dk>
2023-08-06 19:19:33 +0200

    Reload on dropdown select

--- src/lib/Components/nav/DropdownMenu.svelte
+++ src/lib/Components/nav/DropdownMenu.svelte
@@ -15,8 +15,14 @@
 	<span>↓</span>
 </button>
 <div class="card w-48 shadow-xl py-2" data-popup="popupCombobox-{uniqueId}">
-	<a href="/play/3x3x3" class="px-4 py-2 hover:variant-soft w-full block">New 3x3x3 game</a>
-	<a href="/play/5x5x5" class="px-4 py-2 hover:variant-soft w-full block">New 5x5x5 game</a>
-	<a href="/play/10x10x10" class="px-4 py-2 hover:variant-soft w-full block">New 10x10x10 game</a>
+	<a href="/play/3x3x3" data-sveltekit-reload class="px-4 py-2 hover:variant-soft w-full block"
+		>New 3x3x3 game</a
+	>
+	<a href="/play/5x5x5" data-sveltekit-reload class="px-4 py-2 hover:variant-soft w-full block"
+		>New 5x5x5 game</a
+	>
+	<a href="/play/10x10x10" data-sveltekit-reload class="px-4 py-2 hover:variant-soft w-full block"
+		>New 10x10x10 game</a
+	>
 	<div class="arrow bg-surface-100-800-token" />
 </div>
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
+import { threeMinifier } from "@yushijinhun/three-minifier-rollup";
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [threeMinifier(),sveltekit()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	},
