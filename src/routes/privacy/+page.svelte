ded6dcaf25bff6bb6d7df9ac4a4dfdf374387be2
Philip Kristian Møller Flyvholm <phif@itu.dk>
2023-08-15 14:56:34 +0200

    Added privacy policy

---
+++ src/routes/privacy/+page.svelte
@@ -0,0 +1,78 @@
+<h1>Privacy Policy for 3DMinesweeper.com</h1>
+
+<h2>1. Introduction</h2>
+<p>
+	Welcome to 3DMinesweeper.com (the "Website"). This Privacy Policy is designed to help you
+	understand how we collect, use, disclose, and safeguard your information when you visit our
+	Website. Please take a moment to read this Privacy Policy carefully. By accessing or using our
+	Website, you consent to the practices described in this Privacy Policy.
+</p>
+
+<h2>2. Information We Collect</h2>
+<p>
+	We only collect anonymous data via Vercel Analytics, as described in their privacy policy
+	available at <a href="https://vercel.com/docs/concepts/analytics/privacy-policy" target="_blank"
+		>https://vercel.com/docs/concepts/analytics/privacy-policy</a
+	>. This anonymous data includes general usage information such as device type, browser type, pages
+	visited, and referral source. No personally identifiable information is collected.
+</p>
+<p>
+	Additionally, we use fonts from fonts.bunny.net for the loading of fonts. Please refer to their
+	privacy policy for information about how they collect and handle data.
+</p>
+
+<h2>3. How We Use Collected Information</h2>
+<p>
+	The anonymous data collected through Vercel Analytics is used to analyze and improve the
+	performance, functionality, and content of our Website. This data helps us understand user
+	behavior and preferences to enhance user experience.
+</p>
+<p>
+	The fonts provided by fonts.bunny.net are used solely for the purpose of improving the visual
+	presentation of our Website's content.
+</p>
+
+<h2>4. Cookies and Similar Technologies</h2>
+<p>
+	We may use cookies or similar technologies to enhance your experience on our Website. Cookies are
+	small pieces of data stored on your device that may include an anonymous unique identifier. You
+	can control the use of cookies at the individual browser level. Please refer to your browser's
+	settings for more information.
+</p>
+
+<h2>5. Third-Party Services</h2>
+<p>
+	Our Website may contain links to third-party websites or services. We are not responsible for the
+	privacy practices or content of these third parties. We encourage you to read the privacy policies
+	of any third-party websites you visit.
+</p>
+
+<h2>6. Data Security</h2>
+<p>
+	We take reasonable measures to protect the information collected through our Website. However,
+	please be aware that no method of transmission or storage is completely secure. We cannot
+	guarantee the absolute security of your data.
+</p>
+
+<h2>7. Children's Privacy</h2>
+<p>
+	Our Website is not intended for individuals under the age of 13. We do not knowingly collect
+	personal information from children under 13 years of age. If you believe we have inadvertently
+	collected information from a child under 13, please contact us to have it removed.
+</p>
+
+<h2>8. Changes to this Privacy Policy</h2>
+<p>
+	We reserve the right to modify this Privacy Policy at any time. Any changes will be effective
+	immediately upon posting the updated Privacy Policy on our Website. Your continued use of our
+	Website after any such changes constitutes your acceptance of the new Privacy Policy.
+</p>
+
+<h2>9. Contact Us</h2>
+<p>
+	If you have any questions or concerns about this Privacy Policy, please contact us at <a
+		href="mailto:philipflyvholm@gmail.com">philipflyvholm@gmail.com</a
+	>.
+</p>
+
+<p>Thank you for visiting 3DMinesweeper.com.</p>
