1d5bac23804cfdc1a8f7f4819e5173b6a1b8e439
Philip Flyvholm <philipflyvholm@gmail.com>
2023-09-05 19:21:27 +0200

    Fixed tutorial overlay

--- src/lib/Components/overlays/TutorialOverlay.svelte
+++ src/lib/Components/overlays/TutorialOverlay.svelte
@@ -1,16 +1,19 @@
 <script lang="ts">
-	import { tutorialSeen } from "$lib/Stores/LocalStorage";
+	import { tutorialSeen } from '$lib/Stores/LocalStorage';
 
-    function close(){
-        $tutorialSeen = 'true';
-    }
+	function close() {
+		$tutorialSeen = 'true';
+	}
 </script>
+
 <div
-	class="absolute top-[0.5rem] left-1/2 translate-x-[-50%] card py-2 px-5 rounded w-[90%] sm:w-[50%] flex flex-col z-50"
+	class="fixed top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] card py-2 px-5 rounded w-[90%] sm:w-[50%] flex flex-col z-50 max-h-[90vh] overflow-auto"
 >
 	<div class="flex justify-between items-center mt-1">
 		<h2 class="vt323 h1 m-0">How to play</h2>
-        <button class="px-3 py-1 variant-ghost-surface rounded-full font-bold" on:click={close}><small>X</small></button>
+		<button class="px-3 py-1 variant-ghost-surface rounded-full font-bold" on:click={close}
+			><small>X</small></button
+		>
 	</div>
 	<h3 class="h4 m-0 mb-1">Clear all cubes without hitting mines.</h3>
 
@@ -31,27 +34,27 @@
 		<div class="flex items-center">
 			<img
 				src="/how-to/cube.png"
-				class="w-[50%] p-3"
+				class="w-[50%] p-3 max-w-[121px]"
 				alt="A 3x3x3 cube with a handfull of cubes revealed showing the either 1 or 2 depending on the amount of adjacent mines"
 			/>
 
-			<div class="w-[50%] flex justify-around flex-col">
-				<p class="mb-1">Revealed cubes show the adjacent mine count</p>
-				<p>The number defines how many adjacent mines there are in 3d</p>
+			<div class="flex-1 flex justify-around flex-col">
+				<p class="mb-1">Revealed cubes show the adjacent mine count.</p>
+				<p>The number defines how many adjacent mines there are in 3d.</p>
 			</div>
 		</div>
 		<div class="flex items-center">
 			<img
 				src="/how-to/cube-flagged.png"
-				class="w-[50%] p-3"
+				class="w-[50%] p-3 max-w-[121px]"
 				alt="A 3x3x3 cube with a suspected mine flagged"
 			/>
 
-			<div class="w-[50%] flex justify-around flex-col">
-				<p class="mb-1">Empty blocks have no adjacent mines</p>
-				<p>Suspected mines can be marked by flagging</p>
+			<div class="flex flex-1 justify-around flex-col">
+				<p class="mb-1">Empty blocks have no adjacent mines.</p>
+				<p>Suspected mines can be marked by flagging.</p>
 			</div>
 		</div>
 	</div>
-	<button class="btn variant-filled-primary" on:click={close}>Play!</button>
+	<button class="btn variant-filled-primary my-2" on:click={close}>Play!</button>
 </div>
