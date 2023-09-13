c7d27647c34abee68a7d968790010fc5a873f204
Philip Kristian Møller Flyvholm <phif@itu.dk>
2023-09-13 12:01:11 +0200

    Dep fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
-import { threeMinifier } from "@yushijinhun/three-minifier-rollup";
 
 export default defineConfig({
-	plugins: [threeMinifier(),sveltekit()],
+	plugins: [sveltekit()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	},
